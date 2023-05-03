0e192c66a53a4a803054d4563d172d9aa2e42a0c
unknown <lucas.corradini.2003@gmail.com>
2023-05-02 20:54:29 -0700

    Did some changes in js folder

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ let login = document.querySelector("#login");
 let sign = document.querySelector("#sign")
 let btnSign = document.querySelector("#btnSign");
 let loginForm = document.querySelector("#loginForm")
+let SignForm = document.querySelector("#signForm")
 let password = document.querySelector("#password");
 let email = document.querySelector("#email");
 
 
-// console.log(loginForm);
 
 // Function to show and hide the password
 $('.show').click((e)=>{
     e.preventDefault();
     if($(e.target).text()=="Show Password"){
-        $(password).attr("type","text");
+        $("#password").attr("type","text");
+        $("#1-password").attr("type","text");
+        $("#2-password").attr("type","text");
+
         $(e.target).text("Hide Password");
     }else{
-        $(password).attr("type","password");
+        $("#password").attr("type","password");
+        $("#1-password").attr("type","password");
+        $("#2-password").attr("type","password");
         $(e.target).text("Show Password");
     }
 });
@@ -27,12 +32,6 @@ btnCreate.addEventListener("click", (e)=>{
     e.preventDefault();
     $(login).hide();
     $(sign).show();
-});
-
-btnSign.addEventListener("click",(e)=>{
-    e.preventDefault();
-    $(login).show();
-    $(sign).hide()
 });
 
 // function to storage the user information
@@ -49,15 +48,51 @@ loginForm.addEventListener("submit",(e)=>{
     let passwordInfo = document.querySelector("#password").value;;
     let emailInfo = document.querySelector("#email").value;
 
-    if (emailInfo == "") {
+    if (emailInfo == "" && passwordInfo == "") {
         email.classList.add("inputRed");
-    } else {
-        email.classList.remove("inputRed");
-    }
-
-    if (passwordInfo == "") {
         password.classList.add("inputRed");
     } else {
+        email.classList.remove("inputRed");
         password.classList.remove("inputRed");
     }
 });
+
+// for(let inpt of inputElements) {
+    
+// }   
+
+SignForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if($("#infoName").val() == "" && $("#infoEmail").val() == "" && $("#1-password").val() == "" && $("#2-password").val() == ""){
+        $("#infoName").addClass("inputRed");
+        $("#infoEmail").addClass("inputRed");
+        $("#1-password").addClass("inputRed");
+        $("#2-password").addClass("inputRed");
+    }else{
+        $("#infoName").removeClass("inputRed");
+        $("#infoEmail").removeClass("inputRed");
+        $("#1-password").removeClass("inputRed");
+        $("#2-password").removeClass("inputRed");
+
+    if($("#1-password").val() != $("#2-password").val()){
+        console.log("no");
+        $("#1-password").addClass("inputRed");
+        $("#2-password").addClass("inputRed");
+    }else{
+        console.log("yes");
+        $("#1-password").removeClass("inputRed");
+        $("#2-password").removeClass("inputRed");
+
+    }
+
+
+        // btnSign.innerHTML="Continue";
+        // $(login).show();
+        // $(sign).hide();
+        // btnSign.addEventListener("click",(e)=>{
+        //     e.preventDefault();
+        //     // $(login).show();
+        //     // $(sign).hide();
+        // });
+    }
+})
